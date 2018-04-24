993da239f82ace9b45d2b354492ab373fa2c3812
b.tyshkevych <b.tyshkevych@gmail.com>
2018-04-24 16:34:47 +0300

    debugging

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -99,18 +99,18 @@ html_theme_path = ["_themes", ]
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 
-html_static_path = ['nstatic']
+# html_static_path = ['nstatic']
 
 # -- Debuger for rtd theme ----------------------------------------------------
 # -- See https://rackerlabs.github.io/docs-rackspace/tools/rtd-tables.html ----
 
-# html_static_path = ['_static']
+html_static_path = ['_static']
 
-#html_context = {
-#    'css_files': [
-#        '_static/theme_overrides.css',  # override wide tables in RTD theme
-#        ],
-#     }
+html_context = {
+    'css_files': [
+        '_static/theme_overrides.css',  # override wide tables in RTD theme
+        ],
+     }
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
